0a10d3582907fe2b1789dc8d1e5bd1306a5b336b
hafizanwaar <hafizanwaar@paysavo.com>
2026-05-09 15:47:22 +0500

    master_live

--- resources/views/admin/labours/site-vouchers.blade.php
+++ resources/views/admin/labours/site-vouchers.blade.php
@@ -2,24 +2,29 @@
     $selectedSiteName = $selectedSite?->site_name;
 @endphp
 
-@if (!$selectedSite)
-    <tr>
-        <td colspan="7" class="text-center muted">Select a site to view created vouchers.</td>
-    </tr>
-@elseif ($siteVouchers->isEmpty())
-    <tr>
-        <td colspan="7" class="text-center muted">No created vouchers found for {{ $selectedSiteName }}.</td>
-    </tr>
-@else
+
+    @foreach ($siteVouchersNotCreated as $voucher)
+        <tr>
+            <td>--</td>
+            <td>--</td>
+            <td>{{ $voucher->site?->site_name ?? $selectedSiteName ?? 'N/A' }}</td>
+            <td class="right">{{ $voucher->total_amount }}</td>
+            <td>--</td>
+            <td class="tag">Not Created</td>
+            <td class="actions">
+               --
+            </td>
+        </tr>
+    @endforeach
     @foreach ($siteVouchers as $voucher)
         <tr>
             <td>{{ 'JV-' . get_jv_number($voucher->voucher_number) }}</td>
             <td>{{ \Carbon\Carbon::parse($voucher->date)->format('d M Y') }}</td>
-            <td>{{ $voucher->site?->site_name ?? $selectedSiteName ?? 'N/A' }}</td>
+            <td> {{ str_replace('Labour payment for Site: ', '', $voucher->description ?? 'N/A') }}</td>
             <td class="right">{{ number_format((float) $voucher->total_debit, 2) }}</td>
             <td>{{ $voucher->creator?->name ?? 'System' }}</td>
             <td>
-                <span class="tag">{{ ucfirst($voucher->status ?? 'pending') }}</span>
+                <span class="tag">{{ ucfirst($voucher->status ? 'Created' : 'Pending') }}</span>
             </td>
             <td class="actions">
                 <a href="{{ route('journal.voucher.print', $voucher->id) }}" target="_blank" class="icon-btn"
@@ -29,4 +34,3 @@
             </td>
         </tr>
     @endforeach
-@endif
